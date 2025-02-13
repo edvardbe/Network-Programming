8c11b8d46760b9a3a9d8b300fb6bfed474ca84fd
edvardbe <edvard5500@gmail.com>
2025-02-13 21:42:38 +0100

    Changes to exercise 3 and some implementation to exercise 4

---
+++ net-prog/Course Work/e3/build/CMakeFiles/webserver.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webserver.dir/webserver.cpp.obj"
+  "CMakeFiles/webserver.dir/webserver.cpp.obj.d"
+  "libwebserver.dll.a"
+  "webserver.exe"
+  "webserver.exe.manifest"
+  "webserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
