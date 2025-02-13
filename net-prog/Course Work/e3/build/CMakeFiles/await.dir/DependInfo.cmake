ee8fd121b3f8a9adce9433662d846b9e35ab86d6
Edvard <edvardee@stud.ntnu.no>
2025-02-13 16:00:57 +0100

    Clean up CMake build and clone exercise 4 template

---
+++ net-prog/Course Work/e3/build/CMakeFiles/await.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/edvardeek/Documents/University/V2025 Semester/Network Programming/net-prog/Course Work/e3/await.cpp" "CMakeFiles/await.dir/await.cpp.o" "gcc" "CMakeFiles/await.dir/await.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
